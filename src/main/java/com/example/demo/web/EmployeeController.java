2dd64e13b5b4bca65957dabd8b9c21bb3931ca75
jmlemoine <jeanmelvinlp27@gmail.com>
2020-07-09 18:09:09 -0400

    Formulario encuesta

---
+++ src/main/java/com/example/demo/web/EmployeeController.java
@@ -0,0 +1,66 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+    public class EmployeeController {
+
+        @RequestMapping("/encuesta")
+        public String klk(){
+            return "encuesta";
+        }
+
+        /*@Autowired
+        private EmployeeService employeeService;
+
+        @RequestMapping(value = {"/", "/home", "/index"})
+        public ModelAndView get() {
+            ModelAndView mav = new ModelAndView("employeesList");
+            List<Employee> list = employeeService.get();
+            mav.addObject("list", list);
+            return mav;
+        }
+
+        @RequestMapping("/openEmployeeView")
+        public ModelAndView openEmployeeAddView() {
+            ModelAndView mav = new ModelAndView("employeesAdd");
+            mav.addObject("employee", new Employee());
+            return mav;
+        }
+
+        @RequestMapping("/save")
+        public ModelAndView save(@ModelAttribute("employee") Employee employeeObj) {
+            ModelAndView mav = new ModelAndView("employeesList");
+            employeeService.save(employeeObj);
+            List<Employee> list = employeeService.get();
+            mav.addObject("list", list);
+            return mav;
+        }
+
+        @RequestMapping("/employee/{id}")
+        public ModelAndView get(@PathVariable("id") int id) {
+            ModelAndView mav = new ModelAndView("employeesAdd");
+            Employee employeeObj = employeeService.get(id);
+            if(employeeObj == null) {
+                throw new RuntimeException("Employee not found"+id);
+            }
+            mav.addObject("employee", employeeObj);
+            return mav;
+        }
+
+        @RequestMapping("/delete/{id}")
+        public ModelAndView delete(@PathVariable("id") int id) {
+            ModelAndView mav = new ModelAndView("employeesList");
+            employeeService.delete(id);
+            List<Employee> list = employeeService.get();
+            mav.addObject("list", list);
+            return mav;
+        }
+    }*/
+
+}
